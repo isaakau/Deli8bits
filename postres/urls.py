39fd2f3ae126fa964e52ac513acec7aadbbc86a9
Isabella <isabella.moreno.bc@gmail.com>
2021-05-19 15:42:43 -0400

    cambios linkeos y funciones

--- postres/urls.py
+++ postres/urls.py
@@ -3,5 +3,5 @@ from .views import home, contacto
 
 urlpatterns = [
     path('',home,name='home'),
-    path('',contacto,name='contacto'),
+    path('/contacto',contacto,name='contacto'),
 ] #este es el primero que se ejecutas cuando sta vacío
